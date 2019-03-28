c6493219961bb9829c9d75f0235529e1c5b9f5b1
roopamjain <roopamjain@catalyst.net.nz>
2019-03-28 13:57:04 +1300

    NEW: RM#62890 add routing for business owner approval page

---
+++ app/src/Controller/BusinessOwnerApprovalPageController.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * This file contains the "BusinessOwnerApprovalPageController" class.
+ *
+ * @category SilverStripe_Project
+ * @package SDLT
+ * @author  Catalyst I.T. SilverStripe Team 2018 <silverstripedev@catalyst.net.nz>
+ * @copyright 2018 Catalyst.Net Ltd
+ * @license https://www.catalyst.net.nz (Commercial)
+ * @link https://www.catalyst.net.nz
+ */
+
+namespace NZTA\SDLT\Controller;
+
+use PageController;
+use SilverStripe\View\Requirements;
+use SilverStripe\Control\Controller;
+use SilverStripe\Control\HTTPRequest;
+use SilverStripe\View\SSViewer;
+
+/**
+ * Class BusinessOwnerApprovalPageController
+ *
+ */
+class BusinessOwnerApprovalPageController extends PageController
+{
+    /**
+     * Pre-process
+     * @return void
+     */
+    protected function init()
+    {
+        parent::init();
+
+        Requirements::javascript('themes/sdlt/dist/js/main.bundle.js');
+    }
+
+    /**
+     * @param HTTPRequest $request HTTPRequest $request
+     *
+     * @return HTTPResponse
+     */
+    public function index(HTTPRequest $request)
+    {
+        return $this->customise([
+            'Layout' => SSViewer::execute_template('BusinessOwnerApprovalTemplate', [])
+        ])->renderWith('Page');
+    }
+}
