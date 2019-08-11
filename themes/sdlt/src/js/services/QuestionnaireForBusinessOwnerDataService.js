1c687fdc882f5896012d3c4b5f97e7287df23679
roopam jain <roopamjian@catalyst.net.nz>
2019-08-12 10:58:34 +1200

    NEW: RM#66526 Changes for front-end json object

--- themes/sdlt/src/js/services/QuestionnaireForBusinessOwnerDataService.js
+++ themes/sdlt/src/js/services/QuestionnaireForBusinessOwnerDataService.js
@@ -30,6 +30,7 @@ query {
     SubmitterEmail,
     QuestionnaireStatus,
     BusinessOwnerApproverName,
+    GQRiskResult,
     Questionnaire {
       ID
       Name
@@ -73,7 +74,6 @@ query {
     }
 
     // @todo : change with real value
-    const riskResults = [{riskName:"Information Disclosure", weights:"20, 30, 50, 60, 100", score:"160", rating:"Critical", color:"#0085ff"}, {riskName:"Loss of Asset Control", weights:"10, 10, 75", score:"75", rating:"Medium", color:"#8B0000"}];
 
     const data: QuestionnaireSubmissionState = {
       siteTitle: _.toString(_.get(json, "data.readSiteConfig.0.Title", "")),
@@ -119,7 +119,7 @@ query {
             };
             return taskSubmission;
           }),
-        riskResults: riskResults
+        riskResults: _.has(submissionJSON, 'GQRiskResult') ? JSON.parse(_.get(submissionJSON, "GQRiskResult", "")) : ""
       },
     };
 
